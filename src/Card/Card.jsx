855fd187aed5d4fcd9bf4a24aa044e5027dab9f5
hollow3k <angadbajaj301206@gmail.com>
2025-11-10 22:01:02 +0530

    card

---
+++ src/Card/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Card.css'
+
+function Card() {
+  return (
+    <div className='card'>
+        <img src='/project1.png' alt='project1' />
+        <p>Gemini CLone</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, esse facilis, alias vitae incidunt vel odit asperiores rerum nihil sequi voluptates tempore mollitia voluptas aspernatur quaerat inventore. Asperiores, ipsa! Repudiandae?</p>
+        <p>Tech Stack : HTML CSS</p>
+    </div>
+  )
+}
+
+export default Card
